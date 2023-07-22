53f71de5bcf1f002ba16bfd54b5c7b323a0bb6b3
Nishat <nishatparween095@gmail.com>
2023-07-22 17:44:20 +0530

    not working

--- components/body.js
+++ components/body.js
@@ -1,8 +1,15 @@
+import { product_list } from "./product-list";
+import { pcard } from "./card";
+
 const Body = () => {
     return(
         <>
-        <div className="container" style="background-image: https://img.freepik.com/free-vector/hand-painted-watercolor-nature-background_23-2148934719.jpg">
-        <h1>Body</h1>
+        <div className="container" >
+          <div className="product-list">
+             {product_list.map((product_list) => {
+                return <pcard{...product_list} key={product_list.id}/>
+             })}
+          </div>
         
         
         </div>
