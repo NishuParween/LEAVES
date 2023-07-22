3d7d97e2b7d8790c218261ef9b1603767af88edf
Nishat <nishatparween095@gmail.com>
2023-07-22 12:41:20 +0530

    css added some errors because of style in container class in body.js

--- components/body.js
+++ components/body.js
@@ -1,7 +1,7 @@
 const Body = () => {
     return(
         <>
-        <div className="container">
+        <div className="container" style="background-image: https://img.freepik.com/free-vector/hand-painted-watercolor-nature-background_23-2148934719.jpg">
         <h1>Body</h1>
         
         
--- components/header.js
+++ components/header.js
@@ -1,59 +1,55 @@
 import { useState } from "react";
 
-const title = () => {
-    <>
-    <a href ="/"></a>
-    <img className="logo"
-         alt="logo"
-         src="https://img.favpng.com/21/7/18/autumn-leaves-drawing-leaf-png-favpng-VaQBk1pFTxj0iv83RDQbP0GHz.jpg"/>
-         </>
-}
-
 
 const Header = () => {
+    const [searchInput,setsearchInput] = useState();
     return(
         <div className="Header">
             <div className="logo">
-            <title />
+            <>
+             <a href ="/"></a>
+             <img className="logo"
+              alt="logo"
+              src="https://img.favpng.com/21/7/18/autumn-leaves-drawing-leaf-png-favpng-VaQBk1pFTxj0iv83RDQbP0GHz.jpg"/>
+              <h1>Leaves</h1>
+            </>
             </div>
 
             <div className="Search">
-                const [searchInput,setsearchInput] = useState();
-                       return(
-                        <>
-                        {
-                            <div className="Search-container">
-                                <input type="text"
-                                       className="search-input"
-                                       placeholder="Searching Something?"
-                                       value={searchInput}
-                                       onChange={(e) => {
-                                        setsearchInput(e.target.value)
-                                       }
-                                    }
+                
+             <>   
+                 {
+                    <div className="Search-container">
+                      <input type="text"
+                             className="search-input"
+                             placeholder="Searching Something?"
+                             value={searchInput}
+                             onChange={(e) => {
+                              setsearchInput(e.target.value)
+                                   }
+                                }
                                 />
-                                <button className="search-btn" onClick={ () => {
-                                    const data= filterData(searchInput,);
-                                    setsearchInput(data);
-                                }
-
-                                }
-                                >Search</button>
-                            </div>
-                        }
-                        </>
-                       )
+                      <button className="search-btn" onClick={ () => {
+                         const data= filterData(searchInput,);
+                         setsearchInput(data);
+                          }
+                        }>Search</button>
+                   </div>
+                 }
+             </>
+        
                       
 
             </div>
 
             <div className="wishlist">
-                <a class="w-list">wishlist</a>
+                <img src="https://assets.ajio.com/static/img/wishlistIcon.svg" alt="wishlistIcon"></img>
+                
             </div>
 
             <div className="authenticate">
-                <a class="login">LogIn</a>
-                <a class="register">SignUp</a>
+                <a class="login">LogIn /</a>
+                <a class="register"> SignUp</a>
             </div>
         </div>
     )
