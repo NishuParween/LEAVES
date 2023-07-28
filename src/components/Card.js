62472657f7eb09aab0be4ec6f0e2de621d7205af
Nishat <nishatparween095@gmail.com>
2023-07-28 21:36:17 +0530

    fixing rating

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './card.css';
 
+// Create a new Rating component
+const Rating = ({ rating }) => {
+  const numberOfLeaves = Math.ceil(rating);
+
+  // Create an array with the required number of leaves
+  const leaves = Array.from({ length: 5 }, (_, index) => (
+    <img
+      key={index}
+      src={index < numberOfLeaves ? `leaf${index + 1}.png` : `empty-leaf.png`}
+      alt={`Leaf ${index + 1}`}
+      className="rating-leaf"
+    />
+  ));
+
+  return <div className="rating-container">{leaves}</div>;
+};
+
+
 export const Card = ({ pname, image, rating, link }) => {
+
     return (
+      <>
       <div className="flex flex-col max-w-sm mx-auto my-4 rounded overflow-hidden shadow-lg increased-opacity bg-white">
         <img className="w-full h-60 object-cover" src={image} alt="product" />
         <div className="px-4 py-2">
           <h2 className="font-bold text-lg mb-1">{pname}</h2>
-          <h3 className="text-black-600 mb-2">Rating: {rating}</h3>
+          <Rating rating={rating} />
         </div>
         <div className="px-4 py-2 flex justify-between">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">
@@ -20,6 +40,7 @@ export const Card = ({ pname, image, rating, link }) => {
           </a>
         </div>
       </div>
+      </>
     );
   };
   
